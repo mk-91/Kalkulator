1475662d4a960e638eaeab7590207d6201f698c6
Mateusz Kurzawa <mateuszk91@op.pl>
2022-09-22 10:32:53 +0200

    2022-09-22 - wyglad zewnetrzny - html i css

---
+++ script.js
@@ -0,0 +1,7 @@
+const liczby = document.querySelectorAll('.liczba');
+const operatory = document.querySelectorAll('.operator');
+const wyczysc = document.querySelector('.wyczysc');
+const usun = document.querySelector('.usun');
+const rownosc = document.querySelector('.rownosc');
+const wynikPoprzednie = document.querySelector('.poprzednie-dzialanie');
+const wynikAktualne = document.querySelector('.aktualne-dzialanie');
